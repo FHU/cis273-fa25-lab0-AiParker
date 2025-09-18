2731e0f9f13d44176673db820af9e209d06c3bb1
Jordan <157633133+AiParker@users.noreply.github.com>
2025-09-18 18:09:56 -0500

    Vector

--- Vector/Program.cs
+++ Vector/Program.cs
@@ -1,10 +1,31 @@
-﻿namespace Vector;
+﻿using System;
 
-public class Program
+namespace Vector
 {
-    static void Main(string[] args)
+    public class Program
     {
+        public static void Main(string[] args)
+        {
+            Vector v1 = new Vector { X = 3, Y = 4 };
+            Vector v2 = new Vector { X = -2, Y = 5 };
 
+            Vector sum = v1 + v2;
+            Vector diff = v1 - v2;
+            double dot = v1 * v2;
+            Vector scaled = v1 * 2;
+            Vector normalized = v1.Normalize();
+            double angle = v1.AngleBetween(v2);
+
+            Console.WriteLine($"v1 = {v1}");
+            Console.WriteLine($"v2 = {v2}");
+            Console.WriteLine($"Sum = {sum}");
+            Console.WriteLine($"Difference = {diff}");
+            Console.WriteLine($"Dot Product = {dot}");
+            Console.WriteLine($"Scaled v1 = {scaled}");
+            Console.WriteLine($"Normalized v1 = {normalized}");
+            Console.WriteLine($"Angle between v1 and v2 = {angle} degrees");
+            Console.WriteLine($"v1 Magnitude = {v1.Magnitude}");
+            Console.WriteLine($"v1 Direction = {v1.Direction} degrees");
+        }
     }
 }
-
--- Vector/Vector.cs
+++ Vector/Vector.cs
@@ -1,110 +1,36 @@
-﻿namespace Vector;
+﻿using System;
 
-public struct Vector
+namespace Vector
 {
-    public double X { get; set; }
-
-    public double Y { get; set; }
-
-    public double Magnitude { get; }
-
-    public double Direction { get; }
-
-
-    // Instance methods 
-    public Vector Add(Vector v)
+    public struct Vector
     {
-        return default;
-    }
-    public Vector Subtract(Vector v)
-    {
-        return default;
-    }
-    public double Dot(Vector v)
-    {
-        return 0.0;
-    }
-    public double AngleBetween(Vector v)
-    {
-        return 0.0;
-    }
+        public double X { get; set; }
+        public double Y { get; set; }
 
-    public Vector Multiply(double scalar)
-    {
-        return default;
+        public double Magnitude => Math.Sqrt(X * X + Y * Y);
+        public double Direction => Math.Atan2(Y, X) * (180.0 / Math.PI);
+
+        public Vector Add(Vector v) => new Vector { X = X + v.X, Y = Y + v.Y };
+        public Vector Subtract(Vector v) => new Vector { X = X - v.X, Y = Y - v.Y };
+        public double Dot(Vector v) => X * v.X + Y * v.Y;
+        public double AngleBetween(Vector v) => Math.Acos(Dot(v) / (Magnitude * v.Magnitude)) * (180.0 / Math.PI);
+        public Vector Multiply(double scalar) => new Vector { X = X * scalar, Y = Y * scalar };
+        public Vector Divide(double scalar) => new Vector { X = X / scalar, Y = Y / scalar };
+        public Vector Normalize() => Divide(Magnitude);
+        public override string ToString() => $"<{X}, {Y}>";
+
+        public static Vector Add(Vector v1, Vector v2) => new Vector { X = v1.X + v2.X, Y = v1.Y + v2.Y };
+        public static Vector Subtract(Vector v1, Vector v2) => new Vector { X = v1.X - v2.X, Y = v1.Y - v2.Y };
+        public static double Dot(Vector v1, Vector v2) => v1.X * v2.X + v1.Y * v2.Y;
+        public static double AngleBetween(Vector v1, Vector v2) => Math.Acos(Dot(v1, v2) / (v1.Magnitude * v2.Magnitude)) * (180.0 / Math.PI);
+        public static Vector Multiply(Vector v, double scalar) => new Vector { X = v.X * scalar, Y = v.Y * scalar };
+        public static Vector Divide(Vector v, double scalar) => new Vector { X = v.X / scalar, Y = v.Y / scalar };
+        public static Vector Normalize(Vector v) => v.Divide(v.Magnitude);
+
+        public static Vector operator +(Vector v1, Vector v2) => Add(v1, v2);
+        public static Vector operator -(Vector v1, Vector v2) => Subtract(v1, v2);
+        public static double operator *(Vector v1, Vector v2) => Dot(v1, v2);
+        public static Vector operator *(Vector v, double scalar) => Multiply(v, scalar);
+        public static Vector operator /(Vector v, double scalar) => Divide(v, scalar);
     }
-
-    public Vector Divide(double scalar)
-    {
-        return default;
-    }
-
-    public Vector Normalize()
-    {
-        return default;
-    }
-
-    // Class (static) methods 
-    public static Vector Add(Vector v1, Vector v2)
-    {
-        return default;
-    }
-
-    public static Vector Subtract(Vector v1, Vector v2)
-    {
-        return default;
-    }
-
-    public static double Dot(Vector v1, Vector v2)
-    {
-        return 0.0;
-    }
-
-    public static double AngleBetween(Vector v1, Vector v2)
-    {
-        return default;
-    }
-
-     public static Vector Multiply(Vector v, double scalar)
-    {
-        return default;
-    }
-
-    public static Vector Divide(Vector v, double scalar)
-    {
-        return default;
-    }
-
-    public static Vector Normalize(Vector v)
-    {
-        return default;
-    }
-
-    // Overloaded operators 
-    public static Vector operator +(Vector v1, Vector v2)
-    {
-        return default;
-    }
-
-    public static Vector operator -(Vector v1, Vector v2)
-    {
-        return default;
-    }
-
-    public static double operator *(Vector v1, Vector v2)
-    {
-        return 0.0;
-    }
-
-    public static Vector operator *(Vector v1, double scalar)
-    {
-        return default;
-    }
-
-     public static Vector operator /(Vector v1, double scalar)
-    {
-        return default;
-    }
-
-
 }
